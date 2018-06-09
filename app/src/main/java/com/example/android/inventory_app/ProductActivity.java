e86e2cde5695c2c2dcc0d03cb0202a0cd7a249ba
Lucia Bolanča <bolancalucia@gmail.com>
2018-06-09 20:33:33 +0200

    Changing Android Manifest, adding menus in xml, adding starter code for phase 2

---
+++ app/src/main/java/com/example/android/inventory_app/ProductActivity.java
@@ -0,0 +1,6 @@
+package com.example.android.inventory_app;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class ProductActivity extends AppCompatActivity{
+}
---
+++ app/src/main/java/com/example/android/inventory_app/StoreProvider.java
@@ -0,0 +1,4 @@
+package com.example.android.inventory_app;
+
+public class StoreProvider {
+}
